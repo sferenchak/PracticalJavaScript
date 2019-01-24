77592dc3680a56cb78aaa06aa34e2be138ef7143
Steve Ferenchak <steveferenchak@gmail.com>
2019-01-24 14:42:17 -0800

    Refactor functions into methods of todoList object

--- README.md
+++ README.md
@@ -13,3 +13,10 @@ This is just a collection of code that is worked on during the Watch and Code Pr
 * It should have a function to add new todos
 * It should have a function to change todos
 * It should have a function to delete todos
+
+## V3 Requirements
+* It should store the todos array on an object
+* It should have a displayTodos method
+* It should have an addTodo method
+* It should have a changeTodo method
+* It should have a deleteTodo method
--- todos.js
+++ todos.js
@@ -1,34 +1,36 @@
-// store todos in array
-var todos = ["item 1", "item 2", "item 3", "item 4"];
+// todoList object
+const todoList = {
+    todos: ["item 1", "item 2", "item 3", "item 4"],
+    displayTodos: function() {
+        console.log("My Todos:", this.todos);
+    },
+    addTodo: function(todo) {
+        this.todos.push(todo);
+        console.log("Added", todo);
+        this.displayTodos();
+    },
+    changeTodo: function(position, newValue) {
+        let oldValue = this.todos[position];
+        this.todos[position] = newValue;
+        console.log("Changed", oldValue, "to", newValue);
+        this.displayTodos();
+    },
+    deleteTodo: function(position) {
+        let oldValue = this.todos[position];
+        this.todos.splice(position, 1);
+        console.log("Deleted", oldValue);
+        this.displayTodos();
+    }
+}
 
 // display todos
-function displayTodos() {
-  console.log("My Todos:", todos);
-}
-displayTodos();
+todoList.displayTodos();
 
 // add new todo
-function addTodo(todo) {
-    todos.push(todo);
-    console.log("Added", todo);
-    displayTodos();
-}
-addTodo("new todo");
+todoList.addTodo("new todo");
 
 // change todo
-function changeTodo(position, newValue) {
-    let oldValue = todos[position];
-    todos[position] = newValue;
-    console.log("Changed", oldValue, "to", newValue);
-    displayTodos();
-}
-changeTodo(1, 'item 2 Updated')
+todoList.changeTodo(1, 'item 2 Updated')
 
 // delete a todo
-function deleteTodo(position) {
-    let oldValue = todos[position];
-    todos.splice(position, 1);
-    console.log("Deleted", oldValue);
-    displayTodos();
-}
-deleteTodo(1);
+todoList.deleteTodo(1);
