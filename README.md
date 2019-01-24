0ccb608304b0f65361ef9fc1234b9f159c43e442
Steve Ferenchak <steveferenchak@gmail.com>
2019-01-24 11:21:36 -0800

    Tie all logic to functions

--- README.md
+++ README.md
@@ -7,3 +7,9 @@ This is just a collection of code that is worked on during the Watch and Code Pr
 * It should have a way to add new todos
 * It should have a way to change a todo
 * It should have a way to delete a todo
+
+## V2 Requirements
+* It should have a function to display todos
+* It should have a function to add new todos
+* It should have a function to change todos
+* It should have a function to delete todos
--- todos.js
+++ todos.js
@@ -1,26 +1,34 @@
 // store todos in array
-var todos = ['item 1', 'item 2', 'item 3', 'item 4'];
+var todos = ["item 1", "item 2", "item 3", "item 4"];
 
 // display todos
-console.log('My Todos:', todos);
+function displayTodos() {
+  console.log("My Todos:", todos);
+}
+displayTodos();
 
 // add new todo
-todos.push('item 5');
-console.log('Added \'item\ 5\'');
+function addTodo(todo) {
+    todos.push(todo);
+    console.log("Added", todo);
+    displayTodos();
+}
+addTodo("new todo");
 
-// display updated list
-console.log('My Todos:', todos);
-
-// update todo
-todos[1] = 'item 2 Updated';
-console.log('Updated \'item 2\'');
-
-// display updated list
-console.log('My Todos:', todos);
+// change todo
+function changeTodo(position, newValue) {
+    let oldValue = todos[position];
+    todos[position] = newValue;
+    console.log("Changed", oldValue, "to", newValue);
+    displayTodos();
+}
+changeTodo(1, 'item 2 Updated')
 
 // delete a todo
-todos.splice(0, 1);
-console.log('Deleted \'item 1\'');
-
-// display updated list
-console.log('My Todos:', todos);
+function deleteTodo(position) {
+    let oldValue = todos[position];
+    todos.splice(position, 1);
+    console.log("Deleted", oldValue);
+    displayTodos();
+}
+deleteTodo(1);
